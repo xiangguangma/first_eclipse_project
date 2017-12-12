d07a8c0443cae1a76bd5c9648ad28ed5093bd336
guangxiangma <1144974876@qq.com>
2017-12-12 16:12:06 +0800

    初始化代码
    作者:马祥光
    
    2017年12月12日16:11:59

---
+++ src/com/baidu/Hello.java
@@ -0,0 +1,19 @@
+package com.baidu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.write("<h1>66666</h1>");
+	}
+}
